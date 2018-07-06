3c9e5b01b65c3043e8dcf307ab01c187f28a13b8
Ghataorhe <b.ghataorhe@gmail.com>
2018-07-05 23:52:12 -0700

    creates Election migration

--- Election-unbox-petshop/contracts/Election.sol
+++ Election-unbox-petshop/contracts/Election.sol
@@ -2,8 +2,10 @@ pragma solidity ^0.4.11;
 
 contract Election {
     //Store the candidate
+
     //Read the candidate name
     string public candidate;
+    
     //constructor run upon migration
     function Election () public {
         candidate = "BSG"; //this is a state variable, accessible to all of contract
