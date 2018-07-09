b34ae85eeb6c8d66644599496ee4b341e9b54262
Ghataorhe <b.ghataorhe@gmail.com>
2018-07-09 14:20:39 -0700

    tests if the contract initializes with the correct candidate information

--- Election-unbox-petshop/test/election.js
+++ Election-unbox-petshop/test/election.js
@@ -14,4 +14,21 @@ contract("Election", function (accounts) {
             assert.equal(count, 2);
         });
     });
+
+    //do the candidates contain the correctly mapped info?
+    it("initializes the candidates with the correct values", function () {
+        return Election.deployed().then(function (instance) {
+            electionInstance = instance;
+            return electionInstance.candidates(1);
+        }).then(function (candidate) {
+            assert.equal(candidate[0], 1, "contains the correct id");
+            assert.equal(candidate[1], "BSG", "contains the correct name");
+            assert.equal(candidate[2], 0, "contains the correct vote count");
+            return electionInstance.candidates(2);
+        }).then(function (candidate) {
+            assert.equal(candidate[0], 2, "contains the correct id");
+            assert.equal(candidate[1], "SSG", "contains the correct name");
+            assert.equal(candidate[2], 0, "contains the correct votes count");
+        });
+    });
 });
