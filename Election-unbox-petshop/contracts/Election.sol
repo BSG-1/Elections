418d4440ee20239a3453905964ae56aec944364d
Ghataorhe <b.ghataorhe@gmail.com>
2018-07-09 13:26:00 -0700

    creates struct for candidate, mapping that assigns key value pairs, and counter-cache that stores votecounts

--- Election-unbox-petshop/contracts/Election.sol
+++ Election-unbox-petshop/contracts/Election.sol
@@ -2,10 +2,19 @@ pragma solidity ^0.4.2;
 
 contract Election {
     //Model a candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
     //store candidates
-    //fetch candidates by ID from storage
+    //fetch candidates by ID from storage 
+     //*mappings are associative arrays that help assign key value pairs */
+    mapping (uint => Candidate) public candidates;
     //keep track of vote counts for candidates
     //store candidates count
+    uint public candidatesCount; //counter-cache
 
     //constructor run upon migration
     function Election () public {
