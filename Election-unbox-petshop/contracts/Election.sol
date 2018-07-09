b1ba58c73e65ec9920de844a2df493ede2dbe826
Ghataorhe <b.ghataorhe@gmail.com>
2018-07-09 13:21:21 -0700

    more

--- Election-unbox-petshop/contracts/Election.sol
+++ Election-unbox-petshop/contracts/Election.sol
@@ -3,11 +3,13 @@ pragma solidity ^0.4.2;
 contract Election {
     //Model a candidate
     //store candidates
-    //fetch candidates by ID
+    //fetch candidates by ID from storage
     //keep track of vote counts for candidates
+    //store candidates count
 
     //constructor run upon migration
     function Election () public {
         candidate = "BSG1"; //this is a state variable, accessible to all of contract
+    
     }
 }
