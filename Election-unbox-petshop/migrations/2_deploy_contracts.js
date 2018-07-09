76c5635e12d583e87fe3770f4b35544291758fb7
Ghataorhe <b.ghataorhe@gmail.com>
2018-07-09 12:55:11 -0700

    comments on artifact

--- Election-unbox-petshop/migrations/2_deploy_contracts.js
+++ Election-unbox-petshop/migrations/2_deploy_contracts.js
@@ -1,3 +1,4 @@
+//artifacts represents an election artifact that represents our contract and truffle can interact with it
 var Election = artifacts.require("./Election.sol");
 
 module.exports = function (deployer) {
